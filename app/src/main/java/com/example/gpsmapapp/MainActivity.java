ee8e14f13ae1ad5c537876202b5911608f95a2bd
PC <kiritoalfaro88@gmail.com>
2024-10-08 20:33:27 -0300

    Actividad con ubicacion en la serena

---
+++ app/src/main/java/com/example/gpsmapapp/MainActivity.java
@@ -0,0 +1,84 @@
+package com.example.gpsmapapp;
+
+import android.Manifest;
+import android.content.pm.PackageManager;
+import android.os.Bundle;
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
+
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.MapView;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.Marker;
+import com.google.android.gms.maps.model.MarkerOptions;
+
+import java.util.HashMap;
+
+public class MainActivity extends AppCompatActivity implements OnMapReadyCallback {
+
+    private MapView mapView;
+    private GoogleMap googleMap;
+
+    private static final LatLng LA_SERENA = new LatLng(-29.9027, -71.2519);
+
+    private HashMap<LatLng, Marker> markers = new HashMap<>();
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        mapView = findViewById(R.id.mapView);
+        mapView.onCreate(savedInstanceState);
+        mapView.getMapAsync(this);
+    }
+
+    @Override
+    public void onMapReady(@NonNull GoogleMap map) {
+        googleMap = map;
+
+        googleMap.moveCamera(CameraUpdateFactory.newLatLngZoom(LA_SERENA, 13));
+
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION)
+                == PackageManager.PERMISSION_GRANTED) {
+            googleMap.setMyLocationEnabled(true);
+        } else {
+            ActivityCompat.requestPermissions(this,
+                    new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, 1);
+        }
+
+        googleMap.setOnMapClickListener(latLng -> {
+
+            if (markers.containsKey(latLng)) {
+                Marker marker = markers.get(latLng);
+                marker.remove();
+                markers.remove(latLng);
+            } else {
+                Marker newMarker = googleMap.addMarker(new MarkerOptions().position(latLng).title("Punto de interés"));
+                markers.put(latLng, newMarker);
+            }
+        });
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        mapView.onResume();
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        mapView.onPause();
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        mapView.onDestroy();
+    }
+}
